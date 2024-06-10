e8f490df44f15a34ec000fac2b9422263361d661
HelenAlabi <alabihelen64@gmail.com>
2024-06-10 09:29:45 +0100

    l

--- src/sections/NavBar/NavBar.jsx
+++ src/sections/NavBar/NavBar.jsx
@@ -29,9 +29,6 @@ const NavBar = () => {
             {isMenuOpen ? '⁙' : '≡'}
           </div>
           <div className= {`navHamMenu ${isMenuOpen ? 'open' : ''}`} id='navHamMenu'>
-            <div className= "hamburger" onClick={menuHandler}>
-              ⁙
-            </div>
             <ul>
               {data.map(item => (
                 <li key={item.id}>
@@ -47,6 +44,47 @@ const NavBar = () => {
 }
 
 export default NavBar;
+
+
+
+
+// import { useState } from 'react'
+// import Logo from '../../assets/logo.png'
+// import data from './data'
+// import './navBar.css'
+
+// const NavBar = () => {
+//   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+//   const menuHandler = () => {
+//     setIsMenuOpen(!isMenuOpen);
+//   };
+
+//   return (
+//     <nav>
+//       <div className= "container navContainer">
+//         <a href="../../../index.html" className='navLog'>
+//           <img src={Logo} alt="Logo" />
+//         </a>
+//         <ul className= 'navMenu' id='navMenu'>
+//           {data.map(item => <li key={item.id}><a href={item.link}>{item.title}</a></li>)}
+//         </ul>
+//         <div className= "hamburgerMenu">
+//           <div className= "hamburger" onClick={menuHandler}>
+//             {isMenuOpen ? '⁙' : '≡'}
+//           </div>
+//           {isMenuOpen && (
+//             <ul className= 'navHamMenu' id='navHamMenu'>
+//               {data.map(item => <li key={item.id}><a href={item.link}>{item.title}</a></li>)}
+//             </ul>
+//           )}
+//         </div>
+//       </div>
+//     </nav>
+//   )
+// }
+
+// export default NavBar;
 
 
 
